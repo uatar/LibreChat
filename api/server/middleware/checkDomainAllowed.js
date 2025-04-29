8b4d46604ddd0b12dc1b5b225f3c0b455073cd57
uatar <uatarcuaburi@yahoo.co.uk>
2025-04-29 13:19:33 +0300

    refactor: update checkDomainAllowed to use centralized paths
    - Replaced hardcoded '/login' redirection with imported `paths` from the new centralized `paths.js`.
    - Improves consistency and subpath support for OAuth domain validation failures.

--- api/server/middleware/checkDomainAllowed.js
+++ api/server/middleware/checkDomainAllowed.js
@@ -1,5 +1,6 @@
 const { isEmailDomainAllowed } = require('~/server/services/domains');
 const { logger } = require('~/config');
+const { fullPaths } = require('~/server/routes/paths');
 
 /**
  * Checks the domain's social login is allowed
@@ -16,7 +17,7 @@ const checkDomainAllowed = async (req, res, next = () => {}) => {
   const email = req?.user?.email;
   if (email && !(await isEmailDomainAllowed(email))) {
     logger.error(`[Social Login] [Social Login not allowed] [Email: ${email}]`);
-    return res.redirect('/login');
+    return res.redirect(fullPaths.login);
   } else {
     return next();
   }
